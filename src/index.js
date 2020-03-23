a365c2aebf996352ff96c24b2684404c3484f215
bounce <johan@waytogo.se>
2020-03-23 21:19:50 +0100

    fixed indentation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import configureStore from './state/store/configureStore'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import configureStore from "./state/store/configureStore";
 import axios from "axios";
 axios.defaults.baseURL = "http://localhost:3000/api/";
 
@@ -12,8 +12,10 @@ const store = configureStore();
 window.store = store;
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
