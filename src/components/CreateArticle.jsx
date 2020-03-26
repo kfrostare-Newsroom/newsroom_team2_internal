e961a38e1dd9f6a16988dfed71c815fe2e935aea
bounce <johan@waytogo.se>
2020-03-26 15:07:16 +0100

    still looking for that checkbox

--- src/components/CreateArticle.jsx
+++ src/components/CreateArticle.jsx
@@ -1,79 +1,89 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { TextArea, TextInput, Button, Form, Box, RadioButtonGroup } from 'grommet'
-import axios from 'axios'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  TextArea,
+  TextInput,
+  Button,
+  Form,
+  Box,
+  CheckBox
+} from "grommet";
+import axios from "axios";
 
 class CreateArticle extends Component {
-
   articleCreation = async event => {
-    let isPremium;
-    if (event.target.access.value === "Premium") {
-      isPremium = false
+    let articleClass;
+    if (event.target.premium.checked === true) {
+      articleClass = "premium";
+    } else {
+      articleClass = "free";
     }
-    else if (event.target.access.value === "Free") {
-      isPremium = false
-    }
-    debugger
-    
+
     try {
-      let response = await axios.post('/articles', {
+      let response = await axios.post("/articles", {
         article: {
           title: event.target.title.value,
           teaser: event.target.teaser.value,
           content: event.target.content.value,
-          premium_article: isPremium
+          article_class: articleClass
         }
-      })
+
+      });
       debugger
       this.props.dispatch({
         type: "ARTICLE_SUBMITTED",
-        payload: { message: response.data.message }})
+        payload: { message: response.data.message }
+      });
     } catch (error) {
-      this.props.dispatch({type: "ARTICLE_SUBMITTED", payload: {message: error.message}})
-      debugger
+      this.props.dispatch({
+        type: "ARTICLE_SUBMITTED",
+        payload: { message: error.message }
+      });
     }
-  }
+  };
 
-  render () {
+  render() {
     return (
       <>
         <h1>Let's create some magic...</h1>
-        <Form className='create-article' onSubmit={this.articleCreation}>
-          <RadioButtonGroup
-              name="access"
-              className="access"
-              id="access"
-              options={["Free", "Premium"]}
-            />
+        <Form className="create-article" onSubmit={this.articleCreation}>
+          <CheckBox
+            name="premium"
+            className="premium"
+            id="premium"
+            label="Premium?"
+          />
+
           <TextInput
-            className='title'
-            placeholder='This is where you write your title'
-            key='title'
-            id='title'
+            className="title"
+            placeholder="This is where you write your title"
+            key="title"
+            id="title"
             required={true}
           />
           <TextArea
-            className='teaser'
-            placeholder='This is where you write your teaser'
-            key='teaser'
-            id='teaser'
+            className="teaser"
+            placeholder="This is where you write your teaser"
+            key="teaser"
+            id="teaser"
             required={true}
           />
           <TextArea
-            className='content'
-            placeholder='This is where you write your content'
-            key='content'
-            id='content'
+            className="content"
+            placeholder="This is where you write your content"
+            key="content"
+            id="content"
             required={true}
           />
-          <Button label='Submit Article' type='submit' />
-          <Button label='Go Back' onClick={() => this.props.dispatch({ type: 'HIDE_CREATE' })} />
+          <Button label="Submit Article" type="submit" />
+          <Button
+            label="Go Back"
+            onClick={() => this.props.dispatch({ type: "HIDE_CREATE" })}
+          />
         </Form>
-        <Box className="message">
-        {this.props.message}
-        </Box>
+        <Box className="message">{this.props.message}</Box>
       </>
-    )
+    );
   }
 }
 
@@ -83,4 +93,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(CreateArticle)
+export default connect(mapStateToProps)(CreateArticle);
