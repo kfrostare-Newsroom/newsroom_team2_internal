dc0471ff0ec3fd79e368e2204ee112f853bc5e6c
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-28 13:59:44 +0100

    remade the test

--- cypress/integration/userCanCreateArticle.feature.js
+++ cypress/integration/userCanCreateArticle.feature.js
@@ -7,7 +7,31 @@ describe("Journalist can", () => {
       url: "http://localhost:3000/api/articles",
       response: "fixture:create_article_response.json"
     })
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/auth/sign_in",
+      response: "fixture:journalist_login.json",
+      headers: {
+        uid: "user@mail.com",
+      }
+    })
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/auth/validate_token?uid=user@mail.com",
+      response: "fixture:journalist_login.json",
+      headers: {
+        uid: "user@mail.com",
+      }
+    });
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com");
+      cy.get("#password").type("password");
+      cy.get("button")
+        .contains("Login")
+        .click();
+    });
   });
+
 
   it("Create Premium article", () => {
     cy.get("button").contains("Create Article").click()
